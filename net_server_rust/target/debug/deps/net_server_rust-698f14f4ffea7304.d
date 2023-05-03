31f17a3ec9443877c1f461b2aecddc917e6857db
JulianRamirezJ <julianarj39000@gmail.com>
2023-05-03 15:41:20 -0500

    Target deleted

--- net_server_rust/target/debug/deps/net_server_rust-698f14f4ffea7304.d
+++
@@ -1,5 +0,0 @@
-/home/julianramirez/NetExchangeBlue/net_server_rust/target/debug/deps/net_server_rust-698f14f4ffea7304: src/main.rs
-
-/home/julianramirez/NetExchangeBlue/net_server_rust/target/debug/deps/net_server_rust-698f14f4ffea7304.d: src/main.rs
-
-src/main.rs:
--- net_server_rust/target/debug/net_server_rust.d
+++
@@ -1,1 +0,0 @@
-/home/julianramirez/NetExchangeBlue/net_server_rust/target/debug/net_server_rust: /home/julianramirez/NetExchangeBlue/net_server_rust/src/main.rs

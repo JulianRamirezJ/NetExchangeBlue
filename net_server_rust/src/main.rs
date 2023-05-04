df0ee0316ddbd79f54aea480d90e2f7fb2cd274f
JulianRamirezJ <julianarj39000@gmail.com>
2023-05-04 16:46:23 -0500

    Paralel implemented for sending messages

--- net_server_rust/src/main.rs
+++ net_server_rust/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
+use rayon::prelude::*;
 use std::sync::mpsc::{Sender, Receiver, channel};
 
 fn send_loop(rx: Receiver<(String,String)>, streams: Arc<Mutex<HashMap<String, TcpStream>>>) -> io::Result<()> {
@@ -10,11 +11,16 @@ fn send_loop(rx: Receiver<(String,String)>, streams: Arc<Mutex<HashMap<String, T
         match rx.recv() {
             Ok((addr, message)) => {
                 let mut streams = streams.lock().unwrap();
-                for (address, stream) in streams.iter_mut() {
+                /**for (address, stream) in streams.iter_mut() {
                     if *address != addr {
                         stream.write_all(message.as_bytes())?;
                     }
-                }
+                }**/
+                streams.par_iter_mut().for_each(|(address, stream)| {
+                    if *address != addr {
+                        stream.write_all(message.as_bytes()).unwrap();
+                    }
+                });  
             }
             Err(_) => continue,
         }
@@ -28,7 +34,6 @@ fn receive_loop(mut stream: TcpStream, tx: Sender<(String,String)>) -> io::Resul
         match stream.read(&mut buffer) {
             Ok(bytes_read) if bytes_read > 0 => {
                 let message = String::from_utf8_lossy(&buffer[..bytes_read]).trim().to_string();
-                //tx.send(message.clone()).unwrap()
                 tx.send((stream.peer_addr()?.to_string(), message.clone())).unwrap();
                 println!("Received message: {}\n", message);
             }
